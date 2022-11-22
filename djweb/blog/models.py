f99aa6b35a523d4f4ef16ba248f5889f7cc7aa0a
abhhi-101 <birdawade11@gmai.com>
2022-11-22 10:09:30 -0500

    created super user

--- djweb/blog/models.py
+++ djweb/blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Posts(models.Model):
+    name = models.CharField(max_length=100)
+    content = models.TextField()
+    date = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
