d0f394f3ff6495bc3a18dc803161da62c2666645
abhhi-101 <birdawade11@gmai.com>
2022-11-23 18:15:03 -0500

    added profile and images

--- djweb/djweb/urls.py
+++ djweb/djweb/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -26,4 +28,7 @@ urlpatterns = [
     path('logout/',auth_views.LogoutView.as_view(template_name='users/logout.html'),name='logout'),
     path('account/', include('users.urls'))
 
-]
+] 
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- djweb/users/admin.py
+++ djweb/users/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Profile
+from blog.models import Posts
+admin.site.register(Profile)
+admin.site.register(Posts)
